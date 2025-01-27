a0a1f75d69aa93dfaad5d6b24ef7cad809648a29
Adrian Polo <adrianpoloper@gmail.com>
2025-01-27 09:51:51 +0100

    Primmer commit en cocheDemo

---
+++ src/main/java/org/example/cochedemo/CocheDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.cochedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocheDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocheDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cochedemo/CocheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cochedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocheDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
