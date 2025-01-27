4e5612fc610d24e2fadc406a110fd91fb33d3beb
Adrian Polo <adrianpoloper@gmail.com>
2025-01-27 10:09:18 +0100

    Primer commit en la rama crudBasicoDemo

---
+++ src/main/java/org/example/crudbasicodemo/CrudBasicoDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.crudbasicodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudBasicoDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crudbasicodemo/CrudBasicoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crudbasicodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBasicoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
